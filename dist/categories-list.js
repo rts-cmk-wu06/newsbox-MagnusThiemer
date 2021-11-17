f5a6d8b054b56435fd38964bf75054ba93d95795
MagnusThiemer <thiemer.jensen@gmail.com>
2021-11-17 15:06:57 +0100

    Add functional fetch for all categories

--- dist/categories-list.js
+++ dist/categories-list.js
@@ -1,11 +1,7 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', function () {
-  var categories = [
-  /* 'world', 'health', 'sports',  */
-  'business'
-  /* , 'travel' */
-  ];
+  var categories = ['world', 'science', 'sports', 'business', 'arts'];
   var visibleCategories;
   var categoriesListElement = document.querySelector('#categoriesList');
 
@@ -14,41 +10,42 @@ window.addEventListener('DOMContentLoaded', function () {
   }
 
   categories.forEach(function (category) {
-    axios.get("https://api.nytimes.com/svc/topstories/v2/".concat(category, ".json?api-key=GJ1TlurjYAYhgVBgJNPPGnQ5rr9rNkm7")).then(function (data) {
+    var url = "https://api.nytimes.com/svc/topstories/v2/".concat(category, ".json?api-key=GJ1TlurjYAYhgVBgJNPPGnQ5rr9rNkm7");
+    axios.get(url).then(function (data) {
+      console.log(category);
       var parentContainer = document.querySelector("#".concat(category, "NewsUlElement"));
       var newsArray = data.data.results;
       newsArray.forEach(function (newsObject) {
-        console.log(newsObject.url);
-        /* console.log(newsObject); */
-
-        var li = document.createElement('li');
-        li.classList.add('Card');
-        var deleteButtonContainer = document.createElement('div');
-        deleteButtonContainer.classList.add('card__save-button', 'flex-row', 'center', 'align-center');
-        var deleteIcon = document.createElement('i');
-        deleteIcon.classList.add('far', 'fa-hdd');
-        deleteButtonContainer.appendChild(deleteIcon);
-        li.appendChild(deleteButtonContainer);
-        var a = document.createElement('a');
-        a.href = newsObject.url;
-        a.classList.add('flex-row', 'align-center');
-        var imgContainer = document.createElement('div');
-        imgContainer.classList.add('card__img-container');
-        var img = document.createElement('img');
-        img.src = newsObject.multimedia[0].url;
-        var article = document.createElement('article');
-        var headline = document.createElement('h3');
-        var p = document.createElement('p');
-        p.textContent = newsObject["abstract"];
-        headline.textContent = newsObject.title;
-        console.log(newsObject);
-        imgContainer.appendChild(img);
-        article.appendChild(headline);
-        article.appendChild(p);
-        a.appendChild(article);
-        a.appendChild(imgContainer);
-        li.appendChild(a);
-        parentContainer.appendChild(li);
+        if (newsObject.section) {
+          console.log(newsObject);
+          var li = document.createElement('li');
+          li.classList.add('Card');
+          var deleteButtonContainer = document.createElement('div');
+          deleteButtonContainer.classList.add('card__save-button', 'flex-row', 'center', 'align-center');
+          var deleteIcon = document.createElement('i');
+          deleteIcon.classList.add('far', 'fa-hdd');
+          deleteButtonContainer.appendChild(deleteIcon);
+          li.appendChild(deleteButtonContainer);
+          var a = document.createElement('a');
+          a.href = newsObject.url;
+          a.classList.add('flex-row', 'align-center');
+          var imgContainer = document.createElement('div');
+          imgContainer.classList.add('card__img-container');
+          var img = document.createElement('img');
+          img.src = newsObject.multimedia[0].url;
+          var article = document.createElement('article');
+          var headline = document.createElement('h3');
+          var p = document.createElement('p');
+          p.textContent = newsObject["abstract"];
+          headline.textContent = newsObject.title;
+          imgContainer.appendChild(img);
+          article.appendChild(headline);
+          article.appendChild(p);
+          a.appendChild(article);
+          a.appendChild(imgContainer);
+          li.appendChild(a);
+          parentContainer.appendChild(li);
+        }
       });
     });
   });
--- js/categories-list.js
+++ js/categories-list.js
@@ -1,6 +1,6 @@
 
 window.addEventListener('DOMContentLoaded', () => {
-    let categories = [/* 'world', 'health', 'sports',  */'business'/* , 'travel' */];
+    let categories = ['world', 'science', 'sports', 'business', 'arts'];
     let visibleCategories;
     const categoriesListElement = document.querySelector('#categoriesList');
 
@@ -9,45 +9,47 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     categories.forEach(category => {
-        axios.get(`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=GJ1TlurjYAYhgVBgJNPPGnQ5rr9rNkm7`)
+        let url = `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=GJ1TlurjYAYhgVBgJNPPGnQ5rr9rNkm7`
+        axios.get(url)
         .then(data => {  
+            console.log(category)
             let parentContainer = document.querySelector(`#${category}NewsUlElement`);
             let newsArray = data.data.results;
             newsArray.forEach(newsObject => {
-                console.log(newsObject.url);
-                /* console.log(newsObject); */
-                let li = document.createElement('li');
-                li.classList.add('Card')
-
-                let deleteButtonContainer = document.createElement('div');
-                deleteButtonContainer.classList.add('card__save-button', 'flex-row', 'center', 'align-center');
-                let deleteIcon = document.createElement('i');
-                deleteIcon.classList.add('far', 'fa-hdd');
-                deleteButtonContainer.appendChild(deleteIcon);
-                li.appendChild(deleteButtonContainer);
-
-                let a = document.createElement('a');
-                a.href = newsObject.url;
-                a.classList.add('flex-row', 'align-center');
-                let imgContainer = document.createElement('div');
-                imgContainer.classList.add('card__img-container')
-                let img = document.createElement('img');
-                img.src = newsObject.multimedia[0].url;
-                let article = document.createElement('article');
-                let headline = document.createElement('h3');
-                let p = document.createElement('p');
-                p.textContent = newsObject.abstract;
-                headline.textContent = newsObject.title;
-                console.log(newsObject);
-
-                imgContainer.appendChild(img);
-                article.appendChild(headline)
-                article.appendChild(p);
-
-                a.appendChild(article)
-                a.appendChild(imgContainer);
-                li.appendChild(a);
-                parentContainer.appendChild(li)
+                if(newsObject.section){
+                    console.log(newsObject);
+                    let li = document.createElement('li');
+                    li.classList.add('Card')
+    
+                    let deleteButtonContainer = document.createElement('div');
+                    deleteButtonContainer.classList.add('card__save-button', 'flex-row', 'center', 'align-center');
+                    let deleteIcon = document.createElement('i');
+                    deleteIcon.classList.add('far', 'fa-hdd');
+                    deleteButtonContainer.appendChild(deleteIcon);
+                    li.appendChild(deleteButtonContainer);
+    
+                    let a = document.createElement('a');
+                    a.href = newsObject.url;
+                    a.classList.add('flex-row', 'align-center');
+                    let imgContainer = document.createElement('div');
+                    imgContainer.classList.add('card__img-container')
+                    let img = document.createElement('img');
+                    img.src = newsObject.multimedia[0].url;
+                    let article = document.createElement('article');
+                    let headline = document.createElement('h3');
+                    let p = document.createElement('p');
+                    p.textContent = newsObject.abstract;
+                    headline.textContent = newsObject.title;
+    
+                    imgContainer.appendChild(img);
+                    article.appendChild(headline)
+                    article.appendChild(p);
+    
+                    a.appendChild(article)
+                    a.appendChild(imgContainer);
+                    li.appendChild(a);
+                    parentContainer.appendChild(li)
+                }
 
             })
         });
