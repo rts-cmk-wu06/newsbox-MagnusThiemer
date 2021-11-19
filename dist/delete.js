46cfd3bccd3978be5b7313e758243b791c0329ad
MagnusThiemer <thiemer.jensen@gmail.com>
2021-11-19 12:28:28 +0100

    Add delete functionality to archive page

---
+++ dist/delete.js
@@ -0,0 +1,36 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', function () {
+  var container = document.querySelector('#categoriesList');
+  var savedArticles = [];
+
+  if (localStorage.getItem('savedArticles')) {
+    savedArticles = JSON.parse(localStorage.getItem('savedArticles'));
+    console.log(savedArticles);
+  }
+
+  container.addEventListener('click', function (event) {
+    if (event.target.classList.contains('card__delete-button')) {
+      save(event.target.parentNode.id);
+    } else if (event.target.classList.contains('card__delete-button-icon')) {
+      save(event.target.parentNode.parentNode.id);
+    }
+  });
+
+  function save(elementId) {
+    var article = document.querySelector("#".concat(elementId));
+    savedArticles = savedArticles.filter(function (article) {
+      return article.id != elementId;
+    });
+    console.log(savedArticles);
+    localStorage.setItem('savedArticles', JSON.stringify(savedArticles));
+    article.classList.add('animate__animated', 'animate__fadeOutDown', 'animate__fast');
+    setTimeout(function () {
+      article.style.height = '0';
+      article.style.padding = '0';
+    }, 400);
+    setTimeout(function () {
+      article.parentNode.removeChild(article);
+    }, 800);
+  }
+});
---
+++ js/delete.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('#categoriesList');
+    let savedArticles = [];
+
+    if(localStorage.getItem('savedArticles')){
+        savedArticles = JSON.parse(localStorage.getItem('savedArticles'));
+        console.log(savedArticles);
+    }
+
+    container.addEventListener('click', event => {
+        if(event.target.classList.contains('card__delete-button')){
+            save(event.target.parentNode.id)
+        } else if (event.target.classList.contains('card__delete-button-icon')){
+            save(event.target.parentNode.parentNode.id)
+        }
+    })
+
+    function save(elementId){
+        let article = document.querySelector(`#${elementId}`);
+        savedArticles = savedArticles.filter(article => article.id != elementId);
+        console.log(savedArticles);
+
+        localStorage.setItem('savedArticles', JSON.stringify(savedArticles));
+
+        article.classList.add('animate__animated', 'animate__fadeOutDown', 'animate__fast');
+        setTimeout(() => {
+            article.style.height = '0';
+            article.style.padding = '0'
+        }, 400)
+        setTimeout(() => {
+            article.parentNode.removeChild(article);
+        }, 800)
+
+    }
+})
